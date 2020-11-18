1f5da370699b849818c5b354d72f7740e58a73fc
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-18 17:40:32 +0100

    started setting up postgres database

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -1,7 +1,11 @@
 import { MikroORM } from '@mikro-orm/core';
 
 const main = async () => {
-  const orm = await MikroORM.init();
+  const orm = await MikroORM.init({
+    dbName: 'lireddit',
+    user: '',
+    password: '',
+  });
 };
 
 main();
