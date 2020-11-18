cfa5eb6752a142ac1aa4bc9e30bc98124f7d03ab
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-18 17:41:04 +0100

    added type and debug configuration

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -3,8 +3,8 @@ import { MikroORM } from '@mikro-orm/core';
 const main = async () => {
   const orm = await MikroORM.init({
     dbName: 'lireddit',
-    user: '',
-    password: '',
+    type: 'postgresql',
+    debug: true,
   });
 };
 
