64afad038b9a19c3be5669bb5fd47eb842f11573
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-19 13:48:23 +0100

    imported path

--- lireddit-server/src/mikro-orm.config.ts
+++ lireddit-server/src/mikro-orm.config.ts
@@ -1,6 +1,7 @@
 import { Post } from './entities/Post';
 import { __prod__ } from './contstants';
 import { MikroORM } from '@mikro-orm/core';
+import path from 'path';
 
 export default {
   migrations: {
