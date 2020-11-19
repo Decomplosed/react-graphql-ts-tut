91b14064be8e1755c6cfded775f93be4d4593eba
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-19 13:00:36 +0100

    export out config

--- lireddit-server/src/mikro-orm.config.ts
+++ lireddit-server/src/mikro-orm.config.ts
@@ -1,2 +1,9 @@
 import { Post } from './entities/Post';
 import { __prod__ } from './contstants';
+
+export default {
+  entities: [Post],
+  dbName: 'lireddit',
+  type: 'postgresql',
+  debug: !__prod__,
+};
