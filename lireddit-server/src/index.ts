d4c307757afa4bf96e11460298fb37ec04344df1
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-19 13:01:08 +0100

    imported in microConfig

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -1,11 +1,10 @@
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './contstants';
 import { Post } from './entities/Post';
+import microConfig from './mikro-orm.config';
 
 const main = async () => {
-  const orm = await MikroORM.init({
-
-  });
+  const orm = await MikroORM.init();
 
   const post = orm.em.create(Post, { title: 'my first post' });
   await orm.em.persistAndFlush(post);
