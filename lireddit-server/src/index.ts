00cb18f783c455e02fee06bb485360e4a544c332
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-19 12:59:24 +0100

    cut the config

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -4,10 +4,7 @@ import { Post } from './entities/Post';
 
 const main = async () => {
   const orm = await MikroORM.init({
-    entities: [Post],
-    dbName: 'lireddit',
-    type: 'postgresql',
-    debug: !__prod__,
+
   });
 
   const post = orm.em.create(Post, { title: 'my first post' });
