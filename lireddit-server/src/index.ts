70f9cab44f45dfa9cd78daec0c7ea849cfd6e376
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-19 13:01:26 +0100

    passed microConfig to init

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -4,7 +4,7 @@ import { Post } from './entities/Post';
 import microConfig from './mikro-orm.config';
 
 const main = async () => {
-  const orm = await MikroORM.init();
+  const orm = await MikroORM.init(microConfig);
 
   const post = orm.em.create(Post, { title: 'my first post' });
   await orm.em.persistAndFlush(post);
