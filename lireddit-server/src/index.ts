c72bb6aad01b6f2b6e217fd584502a0b2bd93ff0
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-20 16:04:02 +0100

    imported express

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -2,6 +2,7 @@ import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './contstants';
 import { Post } from './entities/Post';
 import microConfig from './mikro-orm.config';
+import express from 'express';
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
