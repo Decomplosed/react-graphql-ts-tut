99cc11cab0c5dc6fa9646f9f5f089172d29e3654
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-20 16:19:58 +0100

    imported in HelloResolver

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -4,6 +4,7 @@ import microConfig from './mikro-orm.config';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
