2718cd4576e1eaed33b21e1341afb43d89233b23
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-20 16:28:23 +0100

    imported in Field type

--- lireddit-server/src/entities/Post.ts
+++ lireddit-server/src/entities/Post.ts
@@ -1,5 +1,5 @@
 import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
-import { ObjectType } from 'type-graphql';
+import { ObjectType, Field } from 'type-graphql';
 
 @ObjectType()
 @Entity()
