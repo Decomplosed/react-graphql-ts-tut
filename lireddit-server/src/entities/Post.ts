dc065c6226a9c9512f91cf10bbba17e8ea1c54ba
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-20 16:28:56 +0100

    passed Field type

--- lireddit-server/src/entities/Post.ts
+++ lireddit-server/src/entities/Post.ts
@@ -4,15 +4,19 @@ import { ObjectType, Field } from 'type-graphql';
 @ObjectType()
 @Entity()
 export class Post {
+  @Field()
   @PrimaryKey()
   id!: number;
 
+  @Field()
   @Property({ type: 'date' })
   createdAt = new Date();
 
+  @Field()
   @Property({ type: 'date', onUpdate: () => new Date() })
   updatedAt = new Date();
 
+  @Field()
   @Property({ type: 'text' })
   title!: string;
 }
