9819ce5360b830fecee50b088cbce18e8ff582c6
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-20 16:15:46 +0100

    imported in buildSchema

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -3,6 +3,7 @@ import { __prod__ } from './contstants';
 import microConfig from './mikro-orm.config';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
