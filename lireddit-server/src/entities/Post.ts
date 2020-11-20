c943c2b3085ec3715fea1f1bd28f93360695701a
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-20 16:31:38 +0100

    reverted passing types

--- lireddit-server/src/entities/Post.ts
+++ lireddit-server/src/entities/Post.ts
@@ -1,18 +1,18 @@
 import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
-import { ObjectType, Field, Int, String } from 'type-graphql';
+import { ObjectType, Field } from 'type-graphql';
 
 @ObjectType()
 @Entity()
 export class Post {
-  @Field(() => Int)
+  @Field()
   @PrimaryKey()
   id!: number;
 
-  @Field(() => String)
+  @Field()
   @Property({ type: 'date' })
   createdAt = new Date();
 
-  @Field(() => String)
+  @Field()
   @Property({ type: 'date', onUpdate: () => new Date() })
   updatedAt = new Date();
 
