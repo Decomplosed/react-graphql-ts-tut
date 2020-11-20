dd31cea8adc549e1c91c71d18b3659e10aca1924
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-20 16:14:25 +0100

    imported ApolloServer

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -2,15 +2,13 @@ import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './contstants';
 import microConfig from './mikro-orm.config';
 import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
 
   const app = express();
-  app.get('/', (_, res) => {
-    res.send('hello');
-  });
   app.listen(4000, () => {
     console.log('Server started on localhost:4000');
   });
