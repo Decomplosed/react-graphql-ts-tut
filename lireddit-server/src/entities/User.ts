c786d90348a0f1c41d0b6b430b62bf0e479fa17d
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-22 12:06:03 +0100

    added createAt and updatedAt fields

--- lireddit-server/src/entities/User.ts
+++ lireddit-server/src/entities/User.ts
@@ -3,4 +3,12 @@ import { ObjectType, Field } from 'type-graphql';
 
 @ObjectType()
 @Entity()
-export class User {}
+export class User {
+  @Field(() => String)
+  @Property({ type: 'date' })
+  createdAt = new Date();
+
+  @Field(() => String)
+  @Property({ type: 'date', onUpdate: () => new Date() })
+  updatedAt = new Date();
+}
