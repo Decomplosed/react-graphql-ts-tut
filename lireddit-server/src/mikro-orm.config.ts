5835b032d38846f72b6e117cf24455ce8305ef9b
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-22 12:16:52 +0100

    passed User to entities

--- lireddit-server/src/mikro-orm.config.ts
+++ lireddit-server/src/mikro-orm.config.ts
@@ -9,7 +9,7 @@ export default {
     path: path.join(__dirname, './migrations'),
     pattern: /^[\w-]+\d+\.[tj]s$/,
   },
-  entities: [Post],
+  entities: [Post, User],
   dbName: 'lireddit',
   type: 'postgresql',
   debug: !__prod__,
