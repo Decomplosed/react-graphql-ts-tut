2b49ad0c6ab087278fea7d9adcabb5b71524ddf8
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-27 15:47:18 +0100

    context to be type of MyContext

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -45,7 +45,7 @@ const main = async () => {
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: ({ req, res }) => ({ em: orm.em, req, res }),
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
   });
 
   apolloServer.applyMiddleware({ app });
