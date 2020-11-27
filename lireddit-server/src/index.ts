d6eaeeb47b13389ae818da4ffb75c3d3cb4c3363
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-27 15:43:57 +0100

    sameSite set to lax

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -31,6 +31,7 @@ const main = async () => {
       cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
         httpOnly: true,
+        sameSite: 'lax',
         secure: __prod__,
       },
       secret: 'idjasifjasif',
