4b47d003e4f5e49827c2c07b5e4bcbb8433ca0d8
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-28 14:26:23 +0100

    await em.findOne

--- lireddit-server/src/resolvers/user.ts
+++ lireddit-server/src/resolvers/user.ts
@@ -45,7 +45,7 @@ export class UserResolver {
       return null;
     }
 
-    const user = em.findOne(User, { id: req.session.userId });
+    const user = await em.findOne(User, { id: req.session.userId });
   }
 
   @Mutation(() => UserResponse)
