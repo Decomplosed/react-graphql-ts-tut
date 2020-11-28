721d27e895e861eb665636da094372595e4de596
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-28 14:44:07 +0100

    added some dummy information

--- lireddit-web/src/pages/register.tsx
+++ lireddit-web/src/pages/register.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
-  return ();
-}
+  return <div>Register Page</div>;
+};
 
 export default Register;
