0a6f025a6a7005e5388806b2d08550496c68aa16
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-29 11:11:24 +0100

    export Wrapper as default

--- lireddit-web/src/components/Wrapper.tsx
+++ lireddit-web/src/components/Wrapper.tsx
@@ -2,4 +2,6 @@ import React from 'react';
 
 interface WrapperProps {}
 
-export const Wrapper: React.FC<WrapperProps> = ({}) => {};
+const Wrapper: React.FC<WrapperProps> = ({}) => {};
+
+export default Wrapper;
