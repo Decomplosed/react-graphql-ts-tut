98ff6e2ef4e551a2a2428ae8459fd3c8c2a4ccdc
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-29 11:46:37 +0100

    imported InputHTMLAttributes

--- lireddit-web/src/components/InputField.tsx
+++ lireddit-web/src/components/InputField.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { InputHTMLAttributes } from 'react';
 import { useField } from 'formik';
 
 interface InputFieldProps {}
 
 const InputField: React.FC<InputFieldProps> = ({}) => {
+  const [] = useField();
+
   return;
 };
 
