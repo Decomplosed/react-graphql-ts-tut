ea6957566b747bbf4ada168e13e1f4fe743d59f5
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-01 17:21:06 +0100

    added REGISTER_MUT variable

--- lireddit-web/src/pages/register.tsx
+++ lireddit-web/src/pages/register.tsx
@@ -7,8 +7,23 @@ import { useMutation } from 'urql';
 
 interface registerProps {}
 
+const REGISTER_MUT = `
+  mutation Register($username: String!, $password: String!) {
+    register(options: {username: $username, password: $password}) {
+      errors {
+        field
+        message
+      }
+      user {
+        id
+        username
+      }
+    }
+  }
+`;
+
 const Register: React.FC<registerProps> = ({}) => {
-  const [] = useMutation('');
+  const [] = useMutation(``);
   return (
     <Formik
       initialValues={{ username: '', password: '' }}
