4650f524da30e7884f878f3cf9eda54be92e378a
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-01 09:14:36 +0100

    extracted size and passed label

--- lireddit-web/src/components/InputField.tsx
+++ lireddit-web/src/components/InputField.tsx
@@ -9,16 +9,19 @@ import {
 
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   label: string;
-  placeholder: string;
   name: string;
 };
 
-const InputField: React.FC<InputFieldProps> = (props) => {
+const InputField: React.FC<InputFieldProps> = ({
+  label,
+  size: _,
+  ...props
+}) => {
   const [field, { error }] = useField(props);
 
   return (
     <FormControl isInvalid={!!error}>
-      <FormLabel htmlFor={field.name}>{props.label}</FormLabel>
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
       <Input {...field} {...props} id={field.name} />
       {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
     </FormControl>
