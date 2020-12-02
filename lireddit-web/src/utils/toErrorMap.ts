d7304261f234abc020106320f0fe505cc993851b
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-02 22:09:34 +0100

    set message to matching field

--- lireddit-web/src/utils/toErrorMap.ts
+++ lireddit-web/src/utils/toErrorMap.ts
@@ -3,5 +3,7 @@ import { FieldError } from '../generated/graphql';
 export const toErrorMap = (errors: FieldError[]) => {
   const errorMap: Record<string, string> = {};
 
-  errors.forEach(({ field, message }) => {});
+  errors.forEach(({ field, message }) => {
+    errorMap[field] = message;
+  });
 };
