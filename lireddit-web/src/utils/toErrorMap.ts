d1f432abfe16f227ae8f05cb385a46b39a165c0f
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-02 22:09:04 +0100

    iterate though errors

--- lireddit-web/src/utils/toErrorMap.ts
+++ lireddit-web/src/utils/toErrorMap.ts
@@ -2,4 +2,6 @@ import { FieldError } from '../generated/graphql';
 
 export const toErrorMap = (errors: FieldError[]) => {
   const errorMap: Record<string, string> = {};
+
+  errors.forEach(({ field, message }) => {});
 };
