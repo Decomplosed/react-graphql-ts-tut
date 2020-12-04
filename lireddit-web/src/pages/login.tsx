f19f51ef611c356bc897c3c8968dd9534754ee04
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-04 12:59:43 +0100

    call Router under useRouter

--- lireddit-web/src/pages/login.tsx
+++ lireddit-web/src/pages/login.tsx
@@ -4,4 +4,6 @@ import { Box, Button } from '@chakra-ui/react';
 import { Wrapper } from '../components/Wrapper';
 import { useRouter } from 'next/router';
 
-const Login: React.FC<{}> = () => {};
+const Login: React.FC<{}> = () => {
+  const router = useRouter()
+};
