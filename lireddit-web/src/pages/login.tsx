d307f7b01b5d83d0ec26d7a8f81fa38f3f706709
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-04 13:07:46 +0100

    imported useLoginMutation

--- lireddit-web/src/pages/login.tsx
+++ lireddit-web/src/pages/login.tsx
@@ -3,6 +3,7 @@ import { Formik, Form } from 'formik';
 import { Box, Button } from '@chakra-ui/react';
 import { Wrapper } from '../components/Wrapper';
 import { useRouter } from 'next/router';
+import { useLoginMutation } from '../generated/graphql';
 
 const Login: React.FC<{}> = () => {
   const router = useRouter();
