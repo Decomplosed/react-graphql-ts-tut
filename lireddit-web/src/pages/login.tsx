cea55bda9d00c01046580426e45d1342e9360010
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-04 12:59:31 +0100

    imported in Router

--- lireddit-web/src/pages/login.tsx
+++ lireddit-web/src/pages/login.tsx
@@ -2,5 +2,6 @@ import React from 'react';
 import { Formik, Form } from 'formik';
 import { Box, Button } from '@chakra-ui/react';
 import { Wrapper } from '../components/Wrapper';
+import { useRouter } from 'next/router';
 
 const Login: React.FC<{}> = () => {};
