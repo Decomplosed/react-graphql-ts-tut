8bdf905bd878a3db407a845e5f17b60d6e263b9b
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-04 12:58:21 +0100

    created interface for loginProps

--- lireddit-web/src/pages/login.tsx
+++ lireddit-web/src/pages/login.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
 import { Formik, Form } from 'formik';
 import { Box, Button } from '@chakra-ui/react';
-import {Wrapper} from '../components/Wrapper'
+import { Wrapper } from '../components/Wrapper';
+
+interface loginProps {}
