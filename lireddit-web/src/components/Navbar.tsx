fce588a551abac6e91229b1593b6d93193a2f98f
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-05 11:26:11 +0100

    passed dummy info within template

--- lireddit-web/src/components/Navbar.tsx
+++ lireddit-web/src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { Box } from '@chakra-ui/react';
 interface NavBarProps {}
 
 const NavBar: React.FC<NavBarProps> = ({}) => {
-  return <Box bg='tomato'></Box>;
+  return <Box bg='tomato'>NAVBAR</Box>;
 };
 
 export default NavBar;
