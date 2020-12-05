2df26008f5ba10e81f9276275f0bf7add6b7228a
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-05 11:25:53 +0100

    imported Box

--- lireddit-web/src/components/Navbar.tsx
+++ lireddit-web/src/components/Navbar.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import { Box } from '@chakra-ui/react';
 
 interface NavBarProps {}
 
 const NavBar: React.FC<NavBarProps> = ({}) => {
-  return (
-    <Box bg='tomato'></Box>
-  )
+  return <Box bg='tomato'></Box>;
 };
 
 export default NavBar;
