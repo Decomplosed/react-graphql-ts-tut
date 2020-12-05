eddbd1ab414ce9698225343965b93d281b39d7d7
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-05 11:43:56 +0100

    imported in Button

--- lireddit-web/src/components/Navbar.tsx
+++ lireddit-web/src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Link, Flex } from '@chakra-ui/react';
+import { Box, Link, Flex, Button } from '@chakra-ui/react';
 import NextLink from 'next/link';
 import { useMeQuery } from '../generated/graphql';
 
