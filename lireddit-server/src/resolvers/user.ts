c5608f0eab75d523be79e252e942a36ac3efdcf1
Bart Frackiewicz <decomplosed@gmail.com>
2020-12-07 08:10:52 +0100

    passed COOKIE_NAME to clearCookie

--- lireddit-server/src/resolvers/user.ts
+++ lireddit-server/src/resolvers/user.ts
@@ -144,7 +144,7 @@ export class UserResolver {
   logout(@Ctx() { req, res }: MyContext) {
     return new Promise((resolve) =>
       req.session.destroy((err: any) => {
-        res.clearCookie('qid');
+        res.clearCookie(COOKIE_NAME);
         if (err) {
           console.log(err);
           resolve(false);
